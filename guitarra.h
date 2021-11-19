d1b683736fb8df522df0af41d11c4a32549b35e8
Diego Ernesto Sandoval Vargas <80714349+DiegoSandovalV@users.noreply.github.com>
2021-11-18 19:29:17 -0600

    Update guitarra.h

--- guitarra.h
+++ guitarra.h
@@ -1,4 +1,15 @@
-//Clase Guitarra
+/*
+ * Proyecto A01709113
+ * Sandoval Vargas Diego Ernesto
+ * A01709113
+ * 18/11/2021
+ */
+
+
+/*Clase Guitarra
+ * Contiene los metodos genericos (setters y getters) para el manejo de del objeto en este caso
+ * de la Guitarra. Tambien contienen un metodo que permite saber el porcentaje de ventas de dicho objeto
+*/
 
 #include<string>
 
@@ -6,41 +17,43 @@ using namespace std;
 
 class Guitarra {
 
-    //Atributos
 private:
-    string tipo;
-    string modelo;
     string marca;
+    string modelo;
+    string sonido; //tipo de sonido
     string cuerdas; //tipo de cuerdas
-    int unidades; //Cantidad de producto que se tiene para vender
-    int ventas;
+    string trastes; //tipo de trastes
 
-    //Constructores y metodos
 public:
+    int unidades; //Cantidad de producto que se tiene para vender
+    int ventas;   //La cantidad que se vendio de dicho producto
+
 
     //Constructor vacio
-    Guitarra() : tipo(""), modelo(""),marca(""), cuerdas(""), unidades(0), ventas(0) {};
+    Guitarra() : marca(""), modelo(""), sonido(""), cuerdas(""),trastes(""),unidades(0), ventas(0) {};
 
     //Constructor
-    Guitarra(string tip, string mod,string mar, string cuer, int un, int ven) : tipo(tip), modelo(mod),marca(mar), cuerdas(cuer),
-                                                                     unidades(un), ventas(ven) {};
+    Guitarra(string mar,string mod,string son,string cuer, string tras,int un, int ven) : marca(mar),modelo(mod),sonido(son),cuerdas(cuer),
+                                                                     trastes(tras),unidades(un), ventas(ven) {};
 
 
     //Metodos
 
     //getters
-    string get_tipo();
-    string get_modelo();
     string get_marca();
+    string get_modelo();
+    string get_sonido();
     string get_cuerdas();
+    string get_trastes();
     int get_unidades();
     int get_ventas();
 
     //setters
-    void set_tipo(string);
-    void set_modelo(string);
     void set_marca(string);
+    void set_modelo(string);
+    void set_sonido(string);
     void set_cuerdas(string);
+    void set_trastes(string);
     void set_unidades(int);
     void set_ventas(int);
 
@@ -51,20 +64,24 @@ public:
 
 //Getters
 
-string Guitarra::get_tipo() {
-    return tipo;
+string Guitarra::get_marca() {
+    return marca;
 }
 
 string Guitarra::get_modelo() {
     return modelo;
 }
 
-string Guitarra::get_marca() {
-    return marca;
+string Guitarra::get_sonido() {
+    return sonido;
 }
 
 string Guitarra::get_cuerdas() {
     return cuerdas;
+}
+
+string Guitarra::get_trastes() {
+    return trastes;
 }
 
 int Guitarra::get_unidades() {
@@ -76,21 +93,24 @@ int Guitarra::get_ventas() {
 }
 
 //Setters
-
-void Guitarra::set_tipo(string tip) {
-    tipo = tip;
+void Guitarra::set_marca(string mar) {
+    marca = mar;
 }
 
 void Guitarra::set_modelo(string mod) {
     modelo = mod;
 }
 
-void Guitarra::set_marca(string mar) {
-    marca = mar;
+void Guitarra::set_sonido(string son) {
+    sonido = son;
 }
 
 void Guitarra::set_cuerdas(string cuer) {
     cuerdas = cuer;
+}
+
+void Guitarra::set_trastes(string tras) {
+    trastes = tras;
 }
 
 void Guitarra::set_unidades(int un) {
@@ -101,18 +121,20 @@ void Guitarra::set_ventas(int ven) {
     ventas = ven;
 }
 
+//Metodo que revisa si la cantidad de unidades y ventas estan correctas y regresa el porcentaje de las ventas del objeto
 string Guitarra::porcentaje_ventas(){
 
-    //Se calcula el porcentaje convirtiendo los valores int de ventas y unidades a float,
+    //Se calcula el porcentaje convirtiendo los valores int de ventas y unidades a float
     float p;
     p = (float) ventas / (float) unidades * 100; // se multiplica * 100 para que se vea mejor el numero
 
     if(unidades>ventas) {
         return "El porcentaje de ventas es: " + to_string(p) + "%";
     }else if(unidades==ventas){
-        return "Se han vendido todas las unidades";
+        return "El porcentaje de ventas es: " + to_string(p) + "% \nSe han vendido todas las unidades";
     }
     else{
         return "Posible error en unidades y ventas";
     }
 }
+
