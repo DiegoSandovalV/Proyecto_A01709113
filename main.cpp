ed0b467c92a4e33c198cc11756c33d25ecbe320b
Diego Ernesto Sandoval Vargas <80714349+DiegoSandovalV@users.noreply.github.com>
2021-11-18 19:31:19 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,65 +9,71 @@ using namespace std;
 int main() {
 
     //Prueba de crear objeto de la clase Guitarra
-    Guitarra guitarra1("Electrica", "Fender", "Stratocaster", "Metal", 10, 1);
+    Guitarra guitarra1("Fender","Stratocaster","digital ", "Metal","medium", 10, 1);
 
     //Se mandan a llamar los getters para demostrar su funcionamiento
-    cout << "Guitarra 1: \n" << guitarra1.get_tipo() << endl << guitarra1.get_modelo() << endl << guitarra1.get_marca()
-         << endl << guitarra1.get_cuerdas() << endl << guitarra1.get_unidades() << endl << guitarra1.get_ventas()
+    cout << "Guitarra 1: \n" << guitarra1.get_marca() << endl << guitarra1.get_modelo() << endl << guitarra1.get_sonido()
+         << endl << guitarra1.get_cuerdas() << endl << guitarra1.get_trastes()<<endl<<guitarra1.get_unidades() << endl << guitarra1.get_ventas()
          << endl<<guitarra1.porcentaje_ventas()<<endl;
 
     //Se mandan a llamar los setters para comprobar funcionamiento
-    guitarra1.set_tipo("Acustica");
-    guitarra1.set_marca("Gibson");
-    guitarra1.set_modelo("Ibañez");
+    guitarra1.set_marca("Yamaha");
+    guitarra1.set_modelo("Parlor");
+    guitarra1.set_sonido("Acustico");
     guitarra1.set_cuerdas("Nylon");
+    guitarra1.set_trastes("bajos");
     guitarra1.set_unidades(8);
     guitarra1.set_ventas(3);
+
     //Se vuelven a llamar los getters para revisar si las modificaciones se hicieron correctamente
-    cout << "\nGuitarra 1: \n" << guitarra1.get_tipo() << endl << guitarra1.get_modelo() << endl << guitarra1.get_marca()
-         << endl << guitarra1.get_cuerdas() << endl << guitarra1.get_unidades() << endl << guitarra1.get_ventas()
+    cout << "Guitarra 1: \n" << guitarra1.get_marca() << endl << guitarra1.get_modelo() << endl << guitarra1.get_sonido()
+         << endl << guitarra1.get_cuerdas() << endl << guitarra1.get_trastes()<<endl<<guitarra1.get_unidades() << endl << guitarra1.get_ventas()
          << endl<<guitarra1.porcentaje_ventas()<<endl;
 
 
     //Prueba de crear objeto de la clase Piano
-    Piano piano1("Digital","Yamaha","Modular",15,2);
+    Piano piano1("Yamaha","Grand","Acustico","Sin sintetizador",88,13,2);
 
     //Se mandan a llamar los getters para demostrar su funcionamiento
-    cout << "\nPiano 1: \n" << piano1.get_sonido() <<endl << piano1.get_marca()
-         << endl << piano1.get_sintetizador() << endl << piano1.get_unidades() << endl << piano1.get_ventas()
+    cout << "Piano 1: \n" << piano1.get_marca() << endl << piano1.get_modelo() << endl << piano1.get_sonido()
+         << endl <<piano1.get_sintetizador() << endl << piano1.get_num_teclas()<<endl<<piano1.get_unidades() << endl << piano1.get_ventas()
          << endl<<piano1.porcentaje_ventas()<<endl;
 
     //Se mandan a llamar los setters para comprobar funcionamiento
-    piano1.set_sonido("Acustico");
-    piano1.set_marca("Nux");
-    piano1.set_sintetizador("No tiene");
-    piano1.set_unidades(9);
-    piano1.set_ventas(9);
-
+    piano1.set_marca("Broadwood");
+    piano1.set_modelo("Cottage");
+    piano1.set_sonido("Digital");
+    piano1.set_sintetizador("Modular");
+    piano1.set_num_teclas(71);
+    piano1.set_unidades(8);
+    piano1.set_ventas(3);
     //Se vuelven a llamar los getters para revisar si las modificaciones se hicieron correctamente
-    cout << "\nPiano 1: \n" << piano1.get_sonido() <<endl << piano1.get_marca()
-         << endl << piano1.get_sintetizador() << endl << piano1.get_unidades() << endl << piano1.get_ventas()
+    cout << "Piano 1: \n" << piano1.get_marca() << endl << piano1.get_modelo() << endl << piano1.get_sonido()
+         << endl <<piano1.get_sintetizador() << endl << piano1.get_num_teclas()<<endl<<piano1.get_unidades() << endl << piano1.get_ventas()
          << endl<<piano1.porcentaje_ventas()<<endl;
 
     //Prueba de crear objeto de la clase Piano
-    Violin violin1("roble","yamaha","nylon",10,2);
+    Violin violin1("Yamaha","Artida","Acustico","Nylon","No incluido",8,8);
 
     //Se mandan a llamar los getters para demostrar su funcionamiento
-    cout << "\nViolin 1: \n" << violin1.get_madera() <<endl << violin1.get_marca()
-         << endl << violin1.get_cuerdas() << endl << violin1.get_unidades() << endl << violin1.get_ventas()
+    cout << "Violin 1: \n" << violin1.get_marca() << endl <<violin1.get_modelo() << endl << violin1.get_sonido()
+         << endl << violin1.get_cuerdas() << endl << violin1.get_barbada()<<endl<<violin1.get_unidades() << endl << guitarra1.get_ventas()
          << endl<<violin1.porcentaje_ventas()<<endl;
+
 
     //Se mandan a llamar los setters para comprobar funcionamiento
-    violin1.set_madera("Abedul");
-    violin1.set_marca("Stenor");
-    violin1.set_cuerdas("Nylon");
-    violin1.set_unidades(8);
-    violin1.set_ventas(9);
+    violin1.set_marca("Yamaha");
+    violin1.set_modelo("Braviol");
+    violin1.set_sonido("Hibrido");
+    violin1.set_cuerdas("Metal");
+    violin1.set_barbada("Plastico");
+    violin1.set_unidades(5);
+    violin1.set_ventas(7);
 
     //Se vuelven a llamar los getters para revisar si las modificaciones se hicieron correctamente
-    cout << "\nViolin 1: \n" << violin1.get_madera() <<endl << violin1.get_marca()
-         << endl << violin1.get_cuerdas() << endl << violin1.get_unidades() << endl << violin1.get_ventas()
-         << endl<<violin1.porcentaje_ventas()<<endl;
+    cout << "Violin 1: \n" << violin1.get_marca() << endl <<violin1.get_modelo() << endl << violin1.get_sonido()
+        << endl << violin1.get_cuerdas() << endl << violin1.get_barbada()<<endl<<violin1.get_unidades() << endl << guitarra1.get_ventas()
+        << endl<<violin1.porcentaje_ventas()<<endl;
 
 
     return 0;
