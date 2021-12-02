8fc3c6bb014141b702fa4acfbb99449c3f6c64cc
Diego Ernesto Sandoval Vargas <80714349+DiegoSandovalV@users.noreply.github.com>
2021-12-01 21:38:30 -0600

    Update piano.h

--- piano.h
+++ piano.h
@@ -10,6 +10,9 @@
 * Contiene las variables que diferencian esta subclase de las demas, al combinar estas con las varriables de la clase padre (instrumento)
  * se obtiene un objeto de tipo Piano
 */
+
+#ifndef PIANO_H
+#define PIANO_H
 #include "instrumento.h"
 using namespace std;
 
@@ -79,7 +82,8 @@ void Piano::set_num_teclas(int num) {
 //Metodo para mostrar las caracteristicas del objeto
 string Piano::mostrar() {
     stringstream aux;
-    aux << marca <<" "<< modelo<<" "<< sintetizador<<" " << num_teclas ;
+    aux << marca << " " << modelo << " " << sintetizador << " " << num_teclas << "\n" << porcentaje_ventas() << endl;
     return aux.str();
 }
 
+#endif
