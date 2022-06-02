513accb41efbc016f1aad71b7953988b6b2faab7
Diego Ernesto Sandoval Vargas <80714349+DiegoSandovalV@users.noreply.github.com>
2022-06-02 12:47:34 -0500

    Update guitarra.h

--- guitarra.h
+++ guitarra.h
@@ -1,43 +1,25 @@
-/*
- * Proyecto A01709113
- * Sandoval Vargas Diego Ernesto
- * A01709113
- * 27/05/2022
- */
-
-
-/*Clase Guitarra
-* Contiene las variables que diferencian esta subclase de las demas, al combinar estas con las varriables de la clase padre (instrumento)
- * se obtiene un objeto de tipo Guitarra
-*/
 #ifndef GUITARRA_H
 #define GUITARRA_H
-
 #include "instrumento.h"
 using namespace std;
 
-class Guitarra : public Instrumento{
+
+class Guitarra: public Instrumento{
 
 private:
-
     string cuerdas;
     string trastes;
 
 public:
-
-    //Constructor default
     Guitarra():Instrumento(){
-        cuerdas = "";
-        trastes = "";
+        cuerdas="";
+        trastes="";
     }
 
-    //Constructor
-    Guitarra(string mar, string mod, float pre,string cuer, string tras, int uni, int ven):Instrumento(mar, mod, pre,
-                                                                                                     uni, ven) {
-        cuerdas = cuer;
-        trastes = tras;
+    Guitarra(string mar, string mod, float pre, string cuer, string tras, int uni, int ven):Instrumento(mar,mod,pre,uni,ven){
+        cuerdas=cuer;
+        trastes=tras;
     }
-
 
     //Metodos
 
@@ -59,10 +41,7 @@ public:
         return aux.str();
     }
 
-
 };
-
-
 //Getters
 
 string Guitarra::get_cuerdas() {
