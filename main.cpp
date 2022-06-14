a4d12ae438e0bdb27d21e2fdb89530c28814cf0f
Diego Ernesto Sandoval Vargas <80714349+DiegoSandovalV@users.noreply.github.com>
2022-06-14 09:37:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <string>
-#include "instrumento.h"
 #include "guitarra.h"
 #include "piano.h"
 #include "violin.h"
 #include "paquete.h"
+#include <limits>
 
 using namespace std;
-
+    //Defino el menu que se mostrara
 void menu() {
     cout << "\nMenu: " << endl;
     cout << "1. Mostrar guitarras" << endl;
     cout << "2. Mostrar Pianos" << endl;
     cout << "3. Mostrar Violines" << endl;
     cout << "4. Mostrar paquete" <<endl;
-    cout << "5. Precio final del paquete" << endl;
-    cout << "6. Precio del paquete con cupon" << endl;
-    cout << "0. Salir" << endl;
+    cout << "5. Crear y Asignar Instrumento al paquete" <<endl;
+    cout << "6. Precio final del paquete" << endl;
+    cout << "7. Precio del paquete con cupon" << endl;
+    cout << "0. Salir" << endl <<endl;
 
 }
 
 int main() {
 
-    //defino los apuntadores objetos
+    //defino los apuntadores objetos de ejemplo
     Guitarra* guitarra1;
     Guitarra* guitarra2;
+    //Este ejemplo no esta instanciado
     Guitarra* guitarra3;
 
     Piano* piano1;
@@ -44,7 +46,6 @@ int main() {
 
 
     //Ejemplos de objetos clase Piano
-
     piano1=new Piano("Yamaha", "Grand", 99.4, "Sin sintetizador", 88, 13, 2);
     piano2=new Piano("Nux", "Privia", 158.9, "Modular", 60, 10, 0);
     piano3=new Piano("Casio", "Clasico", 69.99, "Modular despegable", 63, 5, 1);
@@ -60,22 +61,21 @@ int main() {
 
 
     //Se le asignan las guitarras al paquete
-
     paquete1.asigna(guitarra1);
     paquete1.asigna(guitarra2);
-    paquete1.asigna(guitarra3);
+
 
 
     //Se le asignan los Pianos al paquete
     paquete1.asigna(piano1);
     paquete1.asigna(piano2);
-    paquete1.asigna(piano3);
+
 
 
     //Se le asignan los Violines al paquete
     paquete1.asigna(violin1);
     paquete1.asigna(violin2);
-    paquete1.asigna(violin3);
+
 
     //Variable de la opcion del menu
     int op = 1;
@@ -84,15 +84,25 @@ int main() {
     int cupon;
 
     //Ciclo para que el menu no se cierre hasta que el usuario eliga salir
+
     while (op < 9 && op > 0) {
         menu();
 
         //Se lee la opcion
-        cin >> op;
+
+        //Mi companero (Carlos Salguero) me recomendo crear esta condicion para evitar un ciclo infinito en caso de que la entrada
+        //No sea numerica.
+        while (!(cin >> op))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),
+                            '\n');
+
+            cout << "Entrada invalida, intenta de nuevo: ";
+        }
 
 
         //Switch donde el usuario puede elegir la opcion
-
         switch (op) {
 
             //Se muestran las guitarras actuales
@@ -110,17 +120,22 @@ int main() {
                 cout << violin1->mostrar_car() << violin2->mostrar_car() << violin3->mostrar_car();
                 break;
 
-                //Se muestran los isntrumentos del paquete
+                //Se muestran los instrumentos del paquete
             case 4:
                 cout<<paquete1.mostrar_instrumentos();
                 break;
 
-                //Se Muestra el precio total del paquete
+                //El usuario escoge un tipo de instrumento, para asignarle caracteristicas y agregarlo al paquete
             case 5:
-                cout<<paquete1.precio_final();
+                paquete1.crear_asignar();
                 break;
 
+                //Se Muestra el precio total del paquete
             case 6:
+                cout<<paquete1.precio_final();
+                break;
+                //Se muestra el precio total del paquete si el cliente cuenta con un cupon
+            case 7:
                 cout<< "Ingrese cupon: " << endl;
                 cin>>cupon;
                 cout<<paquete1.precio_final(cupon);
